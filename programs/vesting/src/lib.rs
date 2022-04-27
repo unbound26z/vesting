37787fb30112abf59ed7a9b2cf6f5a64ad7512dd
danilo-andjelic <daniloandjelici9@gmail.com>
2022-04-27 20:10:45 +0200

    DA: Test i prva verzija strukture vestment

--- programs/vesting/src/lib.rs
+++ programs/vesting/src/lib.rs
@@ -2,7 +2,8 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program; //dodaje se zbog constrainta za sys_prog::ID
 
 //TODO: PROMENI PROGRAM ID treba 2GxXeKFC6jL6eMj2a1dCn9XFesYp6WrGXq7HDBZtgcPZ
-declare_id!("2GxXeKFC6jL6eMj2a1dCn9XFesYp6WrGXq7HDBZtgcPZ");
+//Drask ID: 2GxXeKFC6jL6eMj2a1dCn9XFesYp6WrGXq7HDBZtgcPZ Danilo ID: DYWdbcaqeXrWqvbTHeRVPZdEuUkm7YUDBErMkE7FajJS
+declare_id!("DYWdbcaqeXrWqvbTHeRVPZdEuUkm7YUDBErMkE7FajJS");
 
 #[program]
 pub mod vesting {
@@ -15,3 +16,10 @@ pub mod vesting {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct vestment {
+    pub author: Pubkey,
+    pub ammount: i32, //DA: kolicina tokena koji se zakljucavaju
+    pub cliff: i64, //DA: cliff izrazen u danima? mozda postoji bolje resenje
+}

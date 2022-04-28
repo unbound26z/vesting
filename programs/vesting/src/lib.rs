6ba3798547bf2a08b50f100144562057e14c068d
Nikola Drašković <draskovic26z@gmail.com>
2022-04-28 12:29:56 +0200

    ND: Napisan rust i prvi test

--- programs/vesting/src/lib.rs
+++ programs/vesting/src/lib.rs
@@ -8,10 +8,54 @@ declare_id!("2GxXeKFC6jL6eMj2a1dCn9XFesYp6WrGXq7HDBZtgcPZ");
 pub mod vesting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn make_vestment(ctx: Context<MakeVestment>,amount:u16,cliff:u16,period:u8) -> Result<()> {
+        let vestment: &mut Account<Vestment> = &mut ctx.accounts.vestment;
+        let vestor: &Signer = &ctx.accounts.vestor;
+        let clock: Clock = Clock::get().unwrap();
+    
+        vestment.author = *vestor.key;
+        vestment.timestamp = clock.unix_timestamp;
+        vestment.amount = amount;
+        vestment.cliff = cliff;
+        vestment.period = period;
+       
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MakeVestment<'info> {
+    #[account(init,payer=vestor,space=Vestment::LEN)] //inituje acc prave velicine
+    pub vestment: Account<'info,Vestment>, //parsuje iz bitova u Vestment struct
+
+    #[account(mut)] //mut da bi smanjili kolko para ima
+    pub vestor: Signer<'info>, //=AccountInfo al mora i da potpise
+
+    ///CHECK
+    #[account(address=system_program::ID)] //da bude valid
+    pub system_program: AccountInfo<'info>//accountInfo daje account u bitovima
+
+}
+
+
+#[account]
+pub struct Vestment {
+    pub author: Pubkey, // ciji vestment
+    pub timestamp: i64, //pocetak cliffa npr.
+    pub amount: u16, //vljd dovoljan uint8
+    pub cliff: u16, //u danima?
+    pub period: u8, //na kolko se otkljucava
+    //pub procent: u8 //koliko posto investmenta se unlockuje po periodu (ne pise u notion)
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const AMOUNT_LENGTH: usize = 2;
+const CLIFF_LENGTH: usize = 2;
+const PERIOD_LENGTH: usize= 1;
+
+impl Vestment {
+    const LEN: usize=DISCRIMINATOR_LENGTH+PUBLIC_KEY_LENGTH+TIMESTAMP_LENGTH+
+    AMOUNT_LENGTH+CLIFF_LENGTH+PERIOD_LENGTH;
+}
--- tests/vesting.ts
+++ tests/vesting.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Vesting } from "../target/types/vesting";
+import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("vesting", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,30 @@ describe("vesting", () => {
 
   const program = anchor.workspace.Vesting as Program<Vesting>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("make a vestment", async () => {
+    // Before sending the transaction to the blockchain.
+    const vestment = anchor.web3.Keypair.generate();
+
+    await program.rpc.makeVestment(100, 12, 1,{
+      accounts: {
+        vestment: vestment.publicKey,
+        vestor: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [vestment],
+    });
+
+    // Fetch the account details of the created tweet.
+    const vestmentAccount = await program.account.vestment.fetch(vestment.publicKey);
+
+    //insure it has the right data
+    assert.equal(
+      vestmentAccount.author.toBase58(),
+      program.provider.wallet.publicKey.toBase58()
+    );
+    assert.equal(vestmentAccount.amount, 100);
+    assert.equal(vestmentAccount.cliff, 12);
+    assert.equal(vestmentAccount.period, 1);
+    assert.ok(vestmentAccount.timestamp);
   });
 });
